17a01edb3c9d93351f1aa32b8dc5ec33b0bbc62c
James <jamesma909@gmail.com>
2025-05-22 15:28:06 -0500

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -291,9 +291,9 @@ require("lazy").setup({
 	},
 
 	--Vim-sleuth
-	{
-		"tpope/vim-sleuth",
-	},
+	--	{
+	--		"tpope/vim-sleuth",
+	--	},
 	-- NOTE: Plugins can also be added by using a table,
 	-- with the first argument being the link and the following
 	-- keys can be used to configure plugin behavior/loading/etc.
