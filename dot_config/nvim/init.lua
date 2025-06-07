11d4f42972f0633dd7b87d9fbe306677b4494560
James <jamesma909@gmail.com>
2025-06-07 15:25:41 -0500

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1090,4 +1090,4 @@ require("lazy").setup({
 --
 -- Set line number color
 vim.api.nvim_set_hl(0, "LineNr", { fg = "#FBAFD2", bold = false })
-vim.api.nvim_set_hl(0, "LineNr", { fg = "white", bold = true })
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "white", bold = true })
