be2a2d0c64ea00559ac2c0db971a1a0196159744
James <jamesma909@gmail.com>
2025-06-08 14:29:23 -0500

    Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -197,11 +197,6 @@ require('lazy').setup({
     },
   },
 
-  -- Neoformat
-  {
-    'sbdchd/neoformat',
-  },
-
   -- Vim-sleuth
   {
     'tpope/vim-sleuth',
@@ -927,8 +922,6 @@ require('lazy').setup({
       },
       indent = { enable = true, disable = { 'ruby' } },
     },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
     --
     --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
