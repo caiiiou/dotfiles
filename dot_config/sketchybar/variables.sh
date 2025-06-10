2748f92d28dd4dd7b1db5e1df10026648999c1bf
James <jamesma909@gmail.com>
2025-06-10 05:40:28 -0500

    Add .config/sketchybar/.DS_Store Add .config/sketchybar/sketchybarrc Add .config/sketchybar/items/battery.sh Add .config/sketchybar/items/calendar.sh Add .config/sketchybar/items/clock.sh Add .config/sketchybar/items/cpu.sh Add .config/sketchybar/items/front_app.sh Add .config/sketchybar/items/spaces.sh Add .config/sketchybar/items/spotify.sh Add .config/sketchybar/items/volume.sh Add .config/sketchybar/plugins/calendar.sh Add .config/sketchybar/plugins/clock.sh Add .config/sketchybar/plugins/cpu.sh Add .config/sketchybar/plugins/front_app.sh Add .config/sketchybar/plugins/power.sh Add .config/sketchybar/plugins/sound.sh Add .config/sketchybar/plugins/space.sh Add .config/sketchybar/plugins/spotify.sh Add .config/sketchybar/variables.sh

---
+++ dot_config/sketchybar/variables.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env sh
+
+# Color Palette
+# Tokyonight Night
+BLACK=0xe6b4befe
+WHITE=0xe6b4befe
+MAGENTA=0xe6b4befe
+BLUE=0xe6b4befe
+CYAN=0xe6b4befe
+GREEN=0xe6b4befe
+YELLOW=0xe6b4befe
+ORANGE=0xe6b4befe
+RED=0xe6b4befe
+BAR_COLOR=0xff1a1b26
+COMMENT=0xff565f89
+
+# Tokyonight Day
+# BLACK=0xffe9e9ed
+# WHITE=0xff3760bf
+# MAGENTA=0xff9854f1
+# BLUE=0xff2e7de9
+# CYAN=0xff007197
+# GREEN=0xff587539
+# YELLOW=0xff8c6c3e
+# ORANGE=0xffb15c00
+# RED=0xfff52a65
+# BAR_COLOR=0xffe1e2e7
+
+TRANSPARENT=0x00000000
+
+# General bar colors
+ICON_COLOR=$WHITE  # Color of all icons
+LABEL_COLOR=$WHITE # Color of all labels
+
+ITEM_DIR="$HOME/.config/sketchybar/items"
+PLUGIN_DIR="$HOME/.config/sketchybar/plugins"
+
+FONT="MesloLGS NF"
+
+PADDINGS=3
+
+POPUP_BORDER_WIDTH=2
+POPUP_CORNER_RADIUS=11
+POPUP_BACKGROUND_COLOR=$BLACK
+POPUP_BORDER_COLOR=$COMMENT
+
+CORNER_RADIUS=15
+BORDER_WIDTH=2
+
+SHADOW=off
