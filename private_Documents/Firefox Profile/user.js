397e0a6a6ab99471b9adf8bf138c030533b35ce2
James <jamesma909@gmail.com>
2025-06-11 07:04:13 -0500

    Add Documents/Firefox Profile/user.js

---
+++ private_Documents/Firefox Profile/user.js
@@ -0,0 +1,25 @@
+// userchrome.css usercontent.css activate
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Fill SVG Color
+user_pref("svg.context-properties.content.enabled", true);
+
+// CSS's `:has()` selector 
+user_pref("layout.css.has-selector.enabled", true);
+user_pref("gfx.canvas.accelerated.cache-size", 512); // default=256; Chrome=512
+user_pref("gfx.content.skia-font-cache-size", 20); // default=5; Chrome=20
+user_pref("browser.cache.disk.enable", false);
+user_pref("browser.tabs.insertAfterCurrent", true);
+
+user_pref("apz.overscroll.enabled", true); // DEFAULT NON-LINUX
+user_pref("general.smoothScroll", true); // DEFAULT
+user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);
+user_pref("general.smoothScroll.msdPhysics.enabled", true);
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 600);
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 650);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS", 25);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio", "2");
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 250);
+user_pref("general.smoothScroll.currentVelocityWeighting", "1");
+user_pref("general.smoothScroll.stopDecelerationWeighting", "1");
+user_pref("mousewheel.default.delta_multiplier_y", 150); // 250-400; adjust this number to your liking
